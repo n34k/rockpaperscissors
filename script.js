8652eaf0d5d1238eae44af65e1481ec1b46a4d81
n34k <109991419+n34k@users.noreply.github.com>
2022-12-31 19:37:56 -0800

    Update script.js
    Add logic for buttons and to update screen and keep score

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Var declaration
+
+let userChoice;
+let compChoice;
+let wins = 0;
+let losses = 0;
+
+// Node declaration
+
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+const result = document.getElementById("result");
+const score = document.getElementById("score");
+
+// Get computer choice function
+
 function getCompChoice() {
     let num = Math.floor(Math.random() * 3) + 1;
     let choice;
@@ -15,71 +32,78 @@ function getCompChoice() {
     return choice;
 }
 
-function play(userChoice, compChoice) {
-    userChoice.toLowerCase();
+// Play round function
+
+function playRound(userChoice, compChoice) {
     if(userChoice == "rock" && compChoice == "Rock") {
-        console.log("Tie.");
+        result.textContent = "The computer chose Rock. It's a tie!";
         return 0;
     }
     else if (userChoice == "rock" && compChoice == "Scissors") {
-        console.log("Rock beats Scissors. You win!");
+        result.textContent = "The computer chose Scissors. Rock beats Scissors. You win!";
         return 1;
     }
     else if (userChoice == "rock" && compChoice == "Paper") {
-        console.log("Paper beats Rock. You lose.");
+        result.textContent = "The computer chose Paper. Paper beats Rock. You lose.";
         return 2;
     }
     else if (userChoice == "paper" && compChoice == "Rock") {
-        console.log("Paper beats Rock. You win!");
+        result.textContent = "The computer chose Rock. Paper beats Rock. You win!";
         return 1;
     }
     else if (userChoice == "paper" && compChoice == "Paper") {
-        console.log("Tie.");
+        result.textContent = "The computer chose Paper. It's a tie.";
         return 0;
     }
     else if (userChoice == "paper" && compChoice == "Scissors") {
-        console.log("Scissors beats Paper. You lose.");
+        result.textContent = "The computer chose Scissors. Scissors beats paper. You lose.";
         return 2;
     }
     else if (userChoice == "scissors" && compChoice == "Rock") {
-        console.log("Rock beats Scissors. You lose.");
+        result.textContent = "The computer chose Rock. Rock beats Scissors. You lose.";
         return 2;
     }
     else if (userChoice == "scissors" && compChoice == "Paper") {
-        console.log("Scissors beats Paper. You win!");
+        result.textContent = "The computer chose Paper. Scissors beats Paper. You win!";
         return 1;
     }
     else if (userChoice == "scissors" && compChoice == "Scissors") {
-        console.log("Tie.");
+        result.textContent = "The computer chose Scissors. It's a tie.";
         return 0;
     }
 }
 
-function playRound() {
-    let wins = 0;
-    let losses = 0;
-    for(let i = 0; i < 5; i++) {
-        let userChoice = prompt("What's your choice?")
-        let compChoice = getCompChoice();
-        switch(play(userChoice, compChoice)) {
-            case 0:
-                i = i - 1;
-                break;
-            case 1:
-                wins = wins + 1;
-                break;
-            case 2:
-                losses = losses + 1;
-                break;
-        }
-        console.log(wins + " wins - " + losses + " losses")
-        }
-    if (wins > losses) {
-        console.log("You won the match!");
-    }
-    else if (losses > wins) {
-        console.log("You lost the match.");
+// Function to keep score
+
+function playMatch() {
+    switch(playRound(userChoice, compChoice)) {
+        case 0:
+            break;
+        case 1:
+            wins = wins + 1;
+            score.textContent = wins + " wins - " + losses + " losses";
+            break;
+        case 2:
+            losses = losses + 1;
+            score.textContent = wins + " wins - " + losses + " losses";
+            break;
     }
 }
 
-playRound();
+// Button functionality
+
+rockBtn.addEventListener("click", function(e) {
+    userChoice = "rock";
+    compChoice = getCompChoice();
+    playMatch();
+});
+paperBtn.addEventListener("click", function(e) {
+    userChoice = "paper";
+    compChoice = getCompChoice();
+    playMatch();
+});
+scissorsBtn.addEventListener("click", function(e) {
+    userChoice = "scissors";
+    compChoice = getCompChoice();
+    playMatch();
+});
